2a595e47a3923efb3a067bf8d8ee58b0367b194f
Nikolas North <nikolasn09@gmail.com>
2022-03-27 11:34:33 -0400

    implement remove for DoublyLinkedList

--- src/data_structures/doubly_linked_list.py
+++ src/data_structures/doubly_linked_list.py
@@ -43,9 +43,10 @@ class DoublyLinkedList:
     def tail(self):
         return self._tail
 
-    def insert(self, data):
+    def insert(self, data, i: int = -1):
         """
-        Inserts data at the end of the list.
+        Inserts data at the i-th position. Defaults to the end of the list.
+        :param i: Index
         :param data: Data
         :return: None
         """
@@ -53,19 +54,44 @@ class DoublyLinkedList:
         if len(self) == 0:
             self._head = node
             self._tail = node
-        else:
+        elif i == -1 or i == len(self):
             self._tail.set_next(node)
             node.set_prev(self._tail)
             self._tail = node
+        else:
+            prev = self._node_prior_to(i)
         self._size += 1
 
     def remove(self, i: int):
         """
         Removes and returns the data stored at the i-th position.
-        :param i:
-        :return:
+        :param i: Index
+        :return: Data stored at the i-th position
         """
-        pass
+        if i < 0 or i > len(self):
+            raise IndexError
+        node = self._head
+        if len(self) == 1:
+            self._head = None
+            self._tail = None
+        elif i == 0:
+            self._head = self._head.next()
+            self._head.set_prev(None)
+            node.set_next(None)
+        elif i == len(self) - 1:
+            node = self._tail
+            self._tail = self._tail.prev()
+            self._tail.set_next(None)
+            node.set_prev(None)
+        else:
+            prev = self._node_prior_to(i)
+            node = prev.next()
+            prev.set_next(node.next())
+            node.next().set_prev(prev)
+            node.set_prev(None)
+            node.set_next(None)
+        self._size -= 1
+        return node.data()
 
     def __len__(self):
         return self._size
@@ -93,7 +119,7 @@ class DoublyLinkedList:
         :param i: Index
         :return: None
         """
-        pass
+        self.remove(i)
 
     def _node_at(self, i: int):
         """
@@ -104,6 +130,19 @@ class DoublyLinkedList:
         if i < 0 or i >= len(self):
             raise IndexError
         node = self._head
-        for i in range(i):
+        for _ in range(i):
+            node = node.next()
+        return node
+
+    def _node_prior_to(self, i: int):
+        """
+        Returns the node prior to the i-th position. If i = 0, then the first node is returned.
+        :param i: Index
+        :return: Node prior to position `i`
+        """
+        if i < 0 or i > len(self):
+            raise IndexError
+        node = self._head
+        for _ in range(i - 1):
             node = node.next()
         return node
--- src/data_structures/doubly_linked_list_test.py
+++ src/data_structures/doubly_linked_list_test.py
@@ -13,7 +13,7 @@ class TestDoublyLinkedList(unittest.TestCase):
             self.assertEqual(dll[i], items[i], f"incorrect value found at position {i}")
         self.assertEqual(len(dll), len(items))
 
-    def test_set(self):
+    def test_update(self):
         dll = DoublyLinkedList()
         items = [100, 200, 300, 400, 500]
         for item in items:
@@ -21,6 +21,22 @@ class TestDoublyLinkedList(unittest.TestCase):
         for i in range(len(items)):
             dll[i] = items[i] * 10
             self.assertEqual(dll[i], items[i] * 10, f"incorrect value found at position {i}")
+
+    def test_remove(self):
+        dll = DoublyLinkedList()
+        items = [100, 200, 300, 400, 500]
+        for item in items:
+            dll.insert(item)
+        # Remove first node
+        self.assertEqual(dll.remove(0), 100)
+        self.assertEqual(dll[0], 200)
+        # Remove last node
+        self.assertEqual(dll.remove(len(dll) - 1), 500)
+        self.assertEqual(dll[len(dll) - 1], 400)
+        # Remove middle node
+        self.assertEqual(dll.remove(1), 300)
+        self.assertEqual(dll[0], 200)
+        self.assertEqual(dll[1], 400)
 
 
 if __name__ == '__main__':
